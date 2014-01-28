9e24e0a3628812bc59c811e63f3e8b391a5c1a48
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-01-28 14:48:08 +0100

    tmp: compile 2

--- src/topp/FeatureFinderCentroided.cpp
+++ src/topp/FeatureFinderCentroided.cpp
@@ -151,10 +151,10 @@ protected:
     setValidFormats_("out", ListUtils::create<String>("featureXML"));
     registerInputFile_("seeds", "<file>", "", "User specified seed list", false);
     setValidFormats_("seeds", ListUtils::create<String>("featureXML"));
-    
+
     registerOutputFile_("out_mzq", "<file>", "", "Optional output file of MzQuantML.", false, true);
     setValidFormats_("out_mzq", ListUtils::create<String>("mzq"));
-    
+
     addEmptyLine_();
 
     registerSubsection_("algorithm", "Algorithm section");
@@ -162,7 +162,7 @@ protected:
 
   Param getSubsectionDefaults_(const String & /*section*/) const
   {
-    return FeatureFinder().getParameters(FeatureFinderAlgorithmPicked<Peak1D, Feature>::getProductName());
+    return FeatureFinder().getParameters(FeatureFinderAlgorithmPicked<Peak1D>::getProductName());
   }
 
   ExitCodes main_(int, const char **)
@@ -205,7 +205,7 @@ protected:
     writeDebug_("Parameters passed to FeatureFinder", feafi_param, 3);
 
     // Apply the feature finder
-    ff.run(FeatureFinderAlgorithmPicked<Peak1D, Feature>::getProductName(), exp, features, feafi_param, seeds);
+    ff.run(FeatureFinderAlgorithmPicked<Peak1D>::getProductName(), exp, features, feafi_param, seeds);
     features.applyMemberFunction(&UniqueIdInterface::setUniqueId);
 
     // DEBUG
@@ -255,7 +255,7 @@ protected:
     }
 
     map_file.store(out, features);
-    
+
     if (!out_mzq.trim().empty())
     {
       MSQuantifications msq(features, exp.getExperimentalSettings(), exp[0].getDataProcessing() );
@@ -263,7 +263,7 @@ protected:
       MzQuantMLFile file;
       file.store(out_mzq, msq);
     }
-    
+
     return EXECUTION_OK;
   }
 
