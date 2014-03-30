6989a03fdef4ac61b30393a4983f6c8bc6b52d24
Hannes Roest <roest@imsb.biol.ethz.ch>
2014-03-30 09:47:23 +0200

    [FIX] forgotten superhirn fixes in pyOpenMS

--- pyOpenMS/create_cpp_extension.py
+++ pyOpenMS/create_cpp_extension.py
@@ -140,12 +140,14 @@ elif sys.platform == "linux2":
     shutil.copy(j(OPEN_MS_BUILD_DIR, "lib", "libOpenMS.so"), "pyopenms")
     shutil.copy(j(OPEN_MS_BUILD_DIR, "lib", "libSuperHirn.so"), "pyopenms")
     shutil.copy(j(OPEN_MS_BUILD_DIR, "lib", "libOpenSwathAlgo.so"), "pyopenms")
+    shutil.copy(j(OPEN_MS_BUILD_DIR, "lib", "libSuperHirn.so"), "pyopenms")
 
 elif sys.platform == "darwin":
 
     shutil.copy(j(OPEN_MS_BUILD_DIR, "lib", "libOpenMS.dylib"), "pyopenms")
     shutil.copy(j(OPEN_MS_BUILD_DIR, "lib", "libSuperHirn.dylib"), "pyopenms")
     shutil.copy(j(OPEN_MS_BUILD_DIR, "lib", "libOpenSwathAlgo.dylib"), "pyopenms")
+    shutil.copy(j(OPEN_MS_BUILD_DIR, "lib", "libSuperHirn.dylib"), "pyopenms")
 
 else:
     print
